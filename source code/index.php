6a6ee002bbb3d886e7a62c0c93c499129dead264
Lim Chin Yan <chinyan_lim@ecics.com.sg>
2021-02-20 23:51:47 +0800

    changed from require once to include once

--- source code/index.php
+++ source code/index.php
@@ -1,17 +1,15 @@
 <?php
-require_once "header.php"
+
+include_once "header.php";
+include_once "products.php";
+include_once "promotion.php";
+include_once "aboutus.php";
+// require_once "partners.php";
+//require_once "testimonials.php";
+include_once "footer.php";
 ?>
 <meta name="application-name" content="ECICS - Your Friendly Neighborhood Insurer"/>
 <meta charset="utf-8">
 <title>ECICS - Your Friendly Neighborhood Insurer</title>
 
-
-<?php
-require_once "products.php";
-require_once "promotion.php";
-require_once "aboutus.php";
-// require_once "partners.php";
-//require_once "testimonials.php";
-require_once "footer.php";
-?>
 
