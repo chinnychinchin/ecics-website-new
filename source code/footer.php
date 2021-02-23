9b0a600a97f949da36956d917036d9c1c7b524e4
Lim Chin Yan <chinyan_lim@ecics.com.sg>
2021-02-23 12:21:59 +0800

    changed promo carousel back to owl carousel

--- source code/footer.php
+++ source code/footer.php
@@ -119,12 +119,9 @@
         integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV"
         crossorigin="anonymous"></script>
 
-<!-- <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script> -->
 
 <!-- owl carousel -->
-<!-- <script src="vendor/owlcarousel/owl.carousel.js"></script>
+<script src="vendor/owlcarousel/owl.carousel.js"></script>
 <script>
     $(document).ready(function () {
         var productsOwl = $('.banner-carousel');
@@ -179,4 +176,4 @@
 
     });
 
-</script> -->
+</script>
